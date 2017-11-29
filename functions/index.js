6c3855f098a67f11e9373d3fa3fec33d28feb753
Mohammed Elalj <simo@elalj.com>
2017-11-28 16:02:53 -0800

    fixed signUpEmail + meta

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const functions = require('firebase-functions');
 const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,21 +7,23 @@ let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
+const url = 'https://docs.google.com/forms/d/e/1FAIpQLSeZLTJVPsAuIEN4zlLEklBNPwvgbmMvT2mWvd7woirphoB78A/formResponse';
+
+app.post('/signUpEmail', (req, res) => {
   const { email, product } = req.body;
   
   const form = {
     'entry.969253353': email,
     'entry.2037215959': product,
   };
-  return request.post('https://docs.google.com/forms/d/e/1FAIpQLSeZLTJVPsAuIEN4zlLEklBNPwvgbmMvT2mWvd7woirphoB78A/formResponse', { form })
-  .then((res) => {
-    res.send('ok');
-  })
-  .catch((e) => {
-    console.log(e);
-    res.send('error');
+  console.log(form);
+  return request.post(url, { form }, (err) => {
+    if (err) {
+      console.log(err);
+      return res.send('error');
+    }
+    return res.send('ok');
   });
 });
 
-exports.signUpEmail = app;
+exports.signUpEmail = functions.https.onRequest(app);
