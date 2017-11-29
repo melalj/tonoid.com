761dbaa13a01a19f2a9e59c3830e4b599d1e52fc
Mohammed Elalj <simo@elalj.com>
2017-11-28 16:27:22 -0800

    finished version for tonoid

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const url = 'https://docs.google.com/forms/d/e/1FAIpQLSeZLTJVPsAuIEN4zlLEklBNPwvgbmMvT2mWvd7woirphoB78A/formResponse';
 
 app.post('/signUpEmail', (req, res) => {
-  const { email, product } = req.body;
+  const { email, product, comment } = req.body;
   
   const form = {
     'entry.969253353': email,
     'entry.2037215959': product,
+    'entry.1479242274': comment,
   };
   console.log(form);
   return request.post(url, { form }, (err) => {
